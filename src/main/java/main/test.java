0a5d2a3a4bf36637872a3fe858b802f99a64f807
ClOVER-QwQ <1970619599@qq.com>
2024-08-04 17:18:44 +0800

    Merge remote-tracking branch 'origin/dev' into dev

--- src/main/java/main/test.java
+++ src/main/java/main/test.java
@@ -2,7 +2,6 @@ package main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 public class test {
     public static void main(String[] args){
