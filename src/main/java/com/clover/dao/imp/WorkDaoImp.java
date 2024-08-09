5db4cd843d10fa7dd29b0164961ceeb10506eb49
ClOVER-QwQ <1970619599@qq.com>
2024-08-09 10:27:15 +0800

    Merge remote-tracking branch 'origin/dev' into dev

--- src/main/java/com/clover/dao/imp/WorkDaoImp.java
+++ src/main/java/com/clover/dao/imp/WorkDaoImp.java
@@ -1,13 +1,24 @@
 package com.clover.dao.imp;
 
 import com.clover.dao.WorkDao;
+import org.springframework.beans.factory.InitializingBean;
 
-public class WorkDaoImp implements WorkDao {
+public class WorkDaoImp implements WorkDao, InitializingBean {
     public WorkDaoImp(){
         System.out.println("workDao IMP");
     }
 
     private void init() {
         System.out.println("workDao init");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
+    }
+
+    @Override
+    public void show() {
+        System.out.println("show");
     }
 }
