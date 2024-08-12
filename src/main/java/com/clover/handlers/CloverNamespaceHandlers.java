dffb25cb3b5a2712070eeb1e7535b64a4c5d9be2
ClOVER-QwQ <1970619599@qq.com>
2024-08-12 19:32:33 +0800

    Merge remote-tracking branch 'origin/dev' into dev

---
+++ src/main/java/com/clover/handlers/CloverNamespaceHandlers.java
@@ -0,0 +1,12 @@
+package com.clover.handlers;
+
+import org.mybatis.spring.config.NamespaceHandler;
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class CloverNamespaceHandlers extends NamespaceHandlerSupport {
+    @Override
+    public void init() {
+        //初始化，一般情况下，个名空间下有多个标签，会在如让方法中为每一个标签注册一个标签解析器
+        this.registerBeanDefinitionParser("annotation-driven",new );
+    }
+}
--- src/main/java/com/clover/service/imp/UserServiceImp.java
+++ src/main/java/com/clover/service/imp/UserServiceImp.java
@@ -1,6 +1,8 @@
 package com.clover.service.imp;
 
 import com.clover.dao.UserDao;
+import com.clover.mapper.UserMapper;
+import com.clover.pojo.User;
 import com.clover.service.UserService;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.EnableMBeanExport;
@@ -10,72 +12,87 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
-public class UserServiceImp implements UserService, InitializingBean {
-    public UserServiceImp(){
-        System.out.println("无参构造");
-    }
+public class UserServiceImp implements UserService{
+    private UserMapper userMapper;
 
-    public UserServiceImp(String name){
-        System.out.println("有参构造");
-        System.out.println(name);
-    }
-
-    public void init(){
-        System.out.println("init");
-    }
-
-    public void destroy(){
-        System.out.println("destroy");
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
     }
 
     @Override
-    public void afterPropertiesSet(){
-        System.out.println("afterPropertiesSet");
+    public void show() {
+        List<User> all =userMapper.findAll();
+        for (User user : all) {
+            System.out.println(user);
+        }
+        userMapper.findAll();
     }
 
-    private List<UserDao> userDaoList;
-
-    public void setUserDaoList(List<UserDao> userDaoList) {
-        this.userDaoList = userDaoList;
-    }
-
-    private List<String> strings;
-
-    public void setStrings(List<String> strings) {
-        this.strings = strings;
-    }
-
-    private Set<String> stringSet;
-
-    public void setStringSet(Set<String> stringSet) {
-        this.stringSet = stringSet;
-    }
-
-    private Set<UserDao> userDaoSet;
-
-    public void setUserDaoSet(Set<UserDao> userDaoSet) {
-        this.userDaoSet = userDaoSet;
-    }
-
-    private Map<String,UserDao> map;
-
-    public void setMap(Map<String, UserDao> map) {
-        this.map = map;
-    }
-
-    private Properties properties;
-
-    public void setProperties(Properties properties) {
-        this.properties = properties;
-    }
-
-    public void show(){
-        System.out.println(strings);
-        System.out.println(userDaoList);
-        System.out.println(stringSet);
-        System.out.println(userDaoSet);
-        System.out.println(map);
-        System.out.println(properties);
-    }
+//    public UserServiceImp(){
+//        System.out.println("无参构造");
+//    }
+//
+//    public UserServiceImp(String name){
+//        System.out.println("有参构造");
+//        System.out.println(name);
+//    }
+//
+//    public void init(){
+//        System.out.println("init");
+//    }
+//
+//    public void destroy(){
+//        System.out.println("destroy");
+//    }
+//
+//    @Override
+//    public void afterPropertiesSet(){
+//        System.out.println("afterPropertiesSet");
+//    }
+//
+//    private List<UserDao> userDaoList;
+//
+//    public void setUserDaoList(List<UserDao> userDaoList) {
+//        this.userDaoList = userDaoList;
+//    }
+//
+//    private List<String> strings;
+//
+//    public void setStrings(List<String> strings) {
+//        this.strings = strings;
+//    }
+//
+//    private Set<String> stringSet;
+//
+//    public void setStringSet(Set<String> stringSet) {
+//        this.stringSet = stringSet;
+//    }
+//
+//    private Set<UserDao> userDaoSet;
+//
+//    public void setUserDaoSet(Set<UserDao> userDaoSet) {
+//        this.userDaoSet = userDaoSet;
+//    }
+//
+//    private Map<String,UserDao> map;
+//
+//    public void setMap(Map<String, UserDao> map) {
+//        this.map = map;
+//    }
+//
+//    private Properties properties;
+//
+//    public void setProperties(Properties properties) {
+//        this.properties = properties;
+//    }
+//
+//    public void show(){
+//        System.out.println(strings);
+//        System.out.println(userDaoList);
+//        System.out.println(stringSet);
+//        System.out.println(userDaoSet);
+//        System.out.println(map);
+//        System.out.println(properties);
+//    }
 
 }
